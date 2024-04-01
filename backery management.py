4f6c31db3c4cba9e99ae35a1e4d5c806a28c6f42
ASIF AHMAD <67062945+asifahmad0@users.noreply.github.com>
2024-04-01 22:48:32 +0530

    Update backery management.py

--- backery management.py
+++ backery management.py
@@ -24,6 +24,38 @@ if option ==1:
        f.write("\nOrder:- ")
        f.write(order)
        f.close 
+
+#for change 
+elif option ==2:
+    print("\nchoose your option")
+    print("Change Name entre:-  1 ")
+    print("Change  Order entre:-  2")
+    print("quiet entre:-  3")
+    optionForchange=int(input("Entre Your Option:- "))
+    
+    if optionForchange ==1:
+        oldName=input("Entre Your Old Name:- ")
+        newName=input("Entre Your Old Name:- ")
+        with open("order.txt","r") as f:
+          data=f.read()
+          new_Ndata = data.replace(oldName,newName)
+        with open("order.txt","w") as f:
+          f.write(new_Ndata)
+
+    elif optionForchange ==2:
+        oldOrder=input("Entre Your Old Order:- ")
+        newOrder=input("Entre Your Old Order:- ")
+        with open("order.txt","r") as f:
+          data=f.read()
+          new_Odata = data.replace(oldOrder,newOrder)
+        with open("order.txt","w") as f:
+          f.write(new_Odata)
+    elif optionForchange==3:
+       quit
+
+
+
+# for view all orders
 elif option ==3:
     with open("order.txt","r") as s:
         viwe=s.read()
