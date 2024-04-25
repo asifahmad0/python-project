cd45f51aaa29b3f1ffddbf238ab5b3a2fd855cf4
asif <asifahma771@gmail.com>
2024-04-25 18:12:29 +0530

    mini project

---
+++ mini_projects/No_tracker.py
@@ -0,0 +1,13 @@
+import phonenumbers
+from phonenumbers import geocoder
+from phonenumbers import carrier
+
+num= "+917761917649"
+
+ch_number= phonenumbers.parse(num, "CH")
+ro_number= phonenumbers.parse(num, "RO")
+
+cuntry=geocoder.description_for_number(ch_number, "en")
+s_provider= carrier.name_for_number(ro_number, "en")
+print(cuntry)
+print(s_provider)
