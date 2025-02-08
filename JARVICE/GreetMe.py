b709a0a3c267856a5d8b9fa1cdd7f5b72fc3a117
ASIF AHMAD <67062945+asifahmad0@users.noreply.github.com>
2025-02-08 11:22:07 +0530

    Update GreetMe.py

--- JARVICE/GreetMe.py
+++ JARVICE/GreetMe.py
@@ -4,7 +4,7 @@ engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[0].id)
 engine.setProperty("rate",200)
-
+ 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
