5e467ef15a02fb79c2fba6f8d4a99e80b19ab33a
ASIF AHMAD <67062945+asifahmad0@users.noreply.github.com>
2025-03-07 12:03:26 +0530

    Update DSA.py

--- DSA.py
+++ DSA.py
@@ -1758,3 +1758,16 @@ total_sum= sum(rep)
 print(total_sum)
 
 """
+
+#fibonacci
+# Time Complaxity = O(2^n)
+# space complaxity = n-1 * 1
+"""
+def fib(n):
+    if n==1 or n==0:
+        return n
+    
+    return fib(n-1) + fib(n-2)
+
+print(fib(6))
+"""
